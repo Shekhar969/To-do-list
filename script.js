0692cff2f698e255e9b2b2e7ed86a9cf71ae26d7
Shekhar969 <shekharrawal96@gmail.com>
2024-09-03 21:09:21 +0545

    Small Changes

--- script.js
+++ script.js
@@ -6,17 +6,18 @@ document.addEventListener("DOMContentLoaded", () => {
   let delBtn = taskContainer.querySelector("#Delete");
   let hidden =document.querySelector(".hidden")
 
-  addBtn.addEventListener("click", () => {
-      hidden.style.display='block';
-      if (userInput.value.trim() !== "") { // Check if input is not empty
-          let newTask = document.createElement("p"); // Create a new paragraph element for the task
-          newTask.textContent = userInput.value; // Set text content to user input
-          taskPara.appendChild(newTask); // Add the new task to the task container
-          userInput.value = ""; // Clear the input field after adding the task
-      }
-  });
+    addBtn.addEventListener("click", () => {
+        hidden.style.display='none';
+        if (userInput.value.trim() !== "") { // Check if input is not empty
+            let newTask = document.createElement("p"); // Create a new paragraph element for the task
+            newTask.textContent = userInput.value; // Set text content to user input
+            taskPara.appendChild(newTask); // Add the new task to the task container
+            userInput.value = ""; // Clear the input field after adding the task
+        }
+    });
 
-  delBtn.addEventListener("click", () => {
-      taskPara.innerHTML = ""; 
-  });
+    delBtn.addEventListener("click", () => {
+        taskPara.innerHTML = "is it printing ra"; 
+    });
+    
 });
